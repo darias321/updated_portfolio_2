5271ba1e5b64cf99721ece488474eb97ecccdbce
Danielle Arias <daniellearias321@gmail.com>
2020-07-30 13:07:25 -0700

    added portfolio carousel

--- js/init.js
+++ js/init.js
@@ -4,3 +4,8 @@
     $(".parallax").parallax();
   }); // end of document ready
 })(jQuery); // end of jQuery name space
+
+$(".carousel.carousel-slider").carousel({
+  fullWidth: true,
+  indicators: true,
+});
